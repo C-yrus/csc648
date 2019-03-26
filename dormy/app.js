0a1eddf5490e69cbd24d81fe8016c6f072ebd7d0
Matt M <matt@puroxy.org>
2019-03-26 12:50:43 -0700

    created initial express app + some basic config

---
+++ dormy/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const createError = require('http-errors');
+const nunjucks = require('nunjucks');
+
+const app = express();
+
+// database setup
+
+// views directory & engine setup
+// i setup nunjucks (https://mozilla.github.io/nunjucks/), LET ME KNOW IF U WANT THIS CHANGED FRONTEND
+nunjucks.configure('views', {
+    express: app,
+    autoescape: true
+});
+app.set('view engine', 'html');
+
+// logger for outputs to console
+app.use(logger('dev'));
+
+// middleware used populate body request property; (express.json())
+// other middleware for encoding and cookies
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+// static file setup
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/', (req, res) => res.render('index'));
+
+// if it made it here then an error occurred, throw 500 error
+app.use((req, res, next) => next(createError(500)));
+
+app.listen(3000, () => console.log(`Express running at http://localhost:3000`));
