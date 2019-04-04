2f5e29ac8d2f064f9faea1fb5425306086a5d28e
Matt M <matt@puroxy.org>
2019-04-03 22:52:03 -0700

    fixed search. added create new listing page; need to add thumbnail upload, store locally on systemgit status

--- dormy/routes/listings.js
+++ dormy/routes/listings.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const multer = require('multer');
+const upload = multer({dest: 'thumbnails/'});
 const listingsController = require('../controllers/listingsController');
 
 const router = express.Router();
 
 router.get('/', listingsController.list);
+router.post('/', upload.single('thumbnail'), listingsController.addNew);
+router.get('/add', listingsController.add);
 
 module.exports = router;
