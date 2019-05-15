48df604d0fe7b533eb426a77a0d7f8d478ae29b3
Cyrus-Riahi <cyrusriahi@gmail.com>
2019-05-15 16:41:02 -0700

    pointed to db server. modified that database to have the new columns from the mm branch and tested through most of the site to make sure it was working. added data retrieval for pending listings, all listings, and users for the admin dashboard. set up page structure and the sidebar for that dashboard's html

--- dormy/controllers/adminController.js
+++ dormy/controllers/adminController.js
@@ -1,8 +1,35 @@
 
-module.exports.dashboard = (req, res) => {
-    res.render('admin/dashboard.html');
-};
+const db = require('../config/database');
 
+// module.exports.dashboard = (req, res) => {
+//     db.any(`SELECT * FROM listings WHERE NOT approved`)
+//         .then(data => {
+//             res.render('admin/dashboard',{
+//             listing: data,
+//         });
+    
+//     })
+//     .catch(err => res.send(`Error retrieving approved listings; ${err}`))
+// };
+
+module.exports.dashboard = (req, res) => {
+    db.task('get-all', t => {
+        return t.batch([
+            t.any(`SELECT * FROM listings WHERE NOT approved`),
+            t.any(`SELECT * FROM listings WHERE approved`),
+            t.any(`SELECT * FROM users`)
+        ])
+    })
+        .then(data => {
+            res.render('admin/dashboard', {
+                user: req.user,
+                pending: data[0],
+                listings: data[1],
+                users: data[2]
+            });
+        })
+        .catch(err => res.send(`Error retrieving listing detail; ${err}`));
+};
 module.exports.login = (req, res) => {
     res.render('admin/login.html');
 };
@@ -11,3 +38,22 @@ module.exports.logout = (req, res) => {
     req.logout();
     res.redirect('/');
 };
+module.exports.accept = (req, res) => {
+
+    db.one(`UPDATE listings SET approved = TRUE WHERE id = ${req.params.id}`)
+    .then(data => {
+        res.render('dashboard', {
+            listing: data,
+        });
+    })
+    .catch(err => res.send(`Error retrieving listing detail; ${err}`));
+}
+module.exports.reject = (req, res) => {
+    db.one(`DELETE FROM listings WHERE id = ${req.params.id}`)
+    .then(data => {
+        res.render('dashboard', {
+            listing: data,
+        });
+    })
+    .catch(err => res.send(`Error retrieving listing detail; ${err}`));
+}
